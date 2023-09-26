83739dff617fb9ce0ccb67b6a0e002e89608e422
Andrew M <136806756+andrewmcs@users.noreply.github.com>
2023-09-26 18:24:59 +0100

    finished exercises 8-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
 
+function removePunctuation(str) {
+    str = str.replace(/[^\w\s\']|_/g, "")
+         .replace(/\s+/g, "");
+    
+    return str;
+}
+
+
+const palindromes = function (candidateString) {
+    candidateString=removePunctuation(candidateString);
+    candidateString=candidateString.toLowerCase();
+    return candidateString==reverseString(candidateString);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function getAge(person) {
+    if (!person.yearOfDeath) {
+        const date = new Date();
+        let year = date.getFullYear();
+        return year - person.yearOfBirth;
+    }
+    else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
 
+const findTheOldest = function (people) {
+
+    return people.reduce((oldest, currentPerson) => { console.log(currentPerson); return getAge(oldest) > getAge(currentPerson) ? oldest : currentPerson });
 };
 
 // Do not edit below this line
